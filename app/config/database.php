d9318cfb9133f915aacf840a97dfcdcdb996cce6
peaceman <nixalio@gmail.com>
2013-12-15 11:59:43 +0100

    modified default database settings

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'edm_market',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
