fa66500db1e1165f214d0977814f8a678a37f753
peaceman <nixalio@gmail.com>
2013-12-15 12:57:42 +0100

    create user_settings table

---
+++ app/database/migrations/2013_12_15_115432_create_user_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserSettingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('user_settings', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->unsignedInteger('user_id');
+			$table->string('name', 16);
+			$table->string('value');
+			$table->timestamps();
+
+			$table->foreign('user_id', 'us_u_fk')
+				->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('user_settings');
+	}
+
+}
