7e5b05a16582cc9c581c16376098c016a38679f3
peaceman <nixalio@gmail.com>
2013-12-15 12:34:36 +0100

    create users and user_addresses table

---
+++ app/database/migrations/2013_12_15_112125_create_user_addresses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAddressesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('user_addresses', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->unsignedInteger('user_id');
+			$table->unsignedInteger('country_id');
+			$table->string('locality');
+			$table->string('postcode');
+			$table->mediumText('address_lines');
+
+			$table->timestamps();
+
+			$table->foreign('user_id')
+				->references('id')->on('users')
+				->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('user_addresses');
+	}
+
+}
