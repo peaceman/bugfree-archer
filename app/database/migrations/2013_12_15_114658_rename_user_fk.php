502279b4913ecac608facb8cd37820d4ced68cc1
peaceman <nixalio@gmail.com>
2013-12-15 12:49:12 +0100

    rename user fk

---
+++ app/database/migrations/2013_12_15_114658_rename_user_fk.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameUserFk extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('user_addresses', function(Blueprint $table)
+		{
+			$table->dropForeign('user_addresses_user_id_foreign');
+			$table->foreign('user_id', 'ua_u_fk')
+				->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('user_addresses', function(Blueprint $table)
+		{
+			$table->dropForeign('ua_u_fk');
+			$table->foreign('user_id')
+				->references('id')->on('users');
+		});
+	}
+
+}
