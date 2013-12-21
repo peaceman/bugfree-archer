2659271fddd53a3f74812f44569acdbf180e7a8c
peaceman <nixalio@gmail.com>
2013-12-21 19:33:19 +0100

    symlink folders of logs and sessions into the shared folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :repo_url, 'git@github.com:peaceman/bugfree-archer.git'
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{app/storage/logs app/storage/sessions}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
