d1a27cae053574a579c8e728797aaf5627882c84
peaceman <nixalio@gmail.com>
2013-12-21 20:02:15 +0100

    task for artisan database migrations via capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 set :application, 'edm-market'
 set :repo_url, 'git@github.com:peaceman/bugfree-archer.git'
 
+SSHKit.config.command_map[:artisan] = '/usr/bin/env php artisan'
+
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # set :deploy_to, '/var/www/my_app'
@@ -26,8 +28,18 @@ namespace :deploy do
   task :new_release_path do
     set_release_path env.timestamp.strftime("%Y-%m-%d_%H-%M-%S")
   end
+  
+  desc 'executes database migrations through laravel artisan'
+  task :migrate_database do
+    on roles(:db), in: :parallel do
+      within release_path do
+        execute :artisan, :migrate
+      end
+    end
+  end
 
   before :starting, 'composer:install_executable'
+  after :updated, 'deploy:migrate_database'
   after :finishing, 'deploy:cleanup'
 
 end
