50fa06a85dbefcf58cdeef5b9c3423ef6080487b
peaceman <nixalio@gmail.com>
2013-12-21 20:01:26 +0100

    determine the application environment from an environment variable

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,10 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('your-machine-name'),
-
-));
+$env = $app->detectEnvironment(function () {
+	$env = getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'local';
+	return $env;
+});
 
 /*
 |--------------------------------------------------------------------------
