b484163abf42fda10a5713238e6b40676e485293
peaceman <nixalio@gmail.com>
2013-12-22 10:50:39 +0100

    creating a tracking url will update the timestamp of the session

--- app/models/UserTrackingVisitedUrl.php
+++ app/models/UserTrackingVisitedUrl.php
@@ -4,6 +4,7 @@ class UserTrackingVisitedUrl extends Eloquent
 {
 	protected $table = 'user_tracking_visited_urls';
 	protected $fillable = ['url'];
+	protected $touches = ['trackingSession'];
 
 	public function trackingSession()
 	{
