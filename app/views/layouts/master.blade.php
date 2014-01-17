3cbc59e722224eb0c865157c1522ce07dc476a95
peaceman <nixalio@gmail.com>
2014-01-17 11:31:27 +0100

    embed selectize.js

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,6 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- Bootstrap -->
 	<link rel="stylesheet" href="{{ asset('/assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/selectize/css/selectize.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/assets/selectize/css/selectize.bootstrap3.css') }}"/>
 	<link rel="stylesheet" href="{{ asset('/assets/styles.css') }}"/>
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -26,6 +28,7 @@
 <script src="//code.jquery.com/jquery.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="{{ asset('/assets/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('/assets/selectize/js/standalone/selectize.js') }}"></script>
 <script src="{{ asset('/assets/scripts.js') }}"></script>
 </body>
 </html>
