2f91f43cc52e21c9fe6aa45206432511de092551
peaceman <nixalio@gmail.com>
2014-04-13 20:18:19 +0200

    grouped admin navbar links

--- app/views/common/admin/navbar.blade.php
+++ app/views/common/admin/navbar.blade.php
@@ -9,16 +9,70 @@
 		</div>
 		<div class="collapse navbar-collapse" id="collapsable-navbar">
 			<ul class="nav navbar-nav">
-				<li><a href="{{ route('admin.users.index') }}">{{ trans('admin.navbar.users') }}</a></li>
-				<li><a href="{{ route('admin.shop-items.index') }}">{{ trans('admin.navbar.shop_items') }}</a></li>
-				<li><a href="{{ route('admin.shop-categories.index') }}">{{ trans('admin.navbar.shop_categories') }}</a></li>
-				<li><a href="{{ route('admin.reviews.index') }}">{{ trans('admin.navbar.reviews') }}</a></li>
-				<li><a href="{{ route('admin.music-genres.index') }}">{{ trans('admin.navbar.music_genres') }}</a></li>
-				<li><a href="{{ route('admin.music-programs.index') }}">{{ trans('admin.navbar.music_programs') }}</a></li>
-				<li><a href="{{ route('admin.music-plugins.index') }}">{{ trans('admin.navbar.music_plugins') }}</a></li>
-				<li><a href="{{ route('admin.music-plugin-banks.index') }}">{{ trans('admin.navbar.music_plugin_banks') }}</a></li>
-				<li><a href="{{ route('admin.resource-files.index') }}">{{ trans('admin.navbar.resource_files') }}</a></li>
-				<li><a href="{{ route('admin.resource-locations.index') }}">{{ trans('admin.navbar.resource_locations') }}</a></li>
+				<li>
+					<a href="{{ route('admin.users.index') }}">
+						{{ trans('admin.navbar.users') }}
+					</a>
+				</li>
+				<li>
+					<a href="{{ route('admin.shop-items.index') }}">
+						{{ trans('admin.navbar.shop_items') }}
+					</a>
+				</li>
+				<li>
+					<a href="{{ route('admin.shop-categories.index') }}">
+						{{ trans('admin.navbar.shop_categories') }}
+					</a>
+				</li>
+				<li>
+					<a href="{{ route('admin.reviews.index') }}">
+						{{ trans('admin.navbar.reviews') }}
+					</a>
+				</li>
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="">
+						{{ trans('admin.navbar.music_software') }} <b class="caret"></b>
+					</a>
+					<ul class="dropdown-menu">
+						<li>
+							<a href="{{ route('admin.music-programs.index') }}">
+								{{ trans('admin.navbar.music_programs') }}
+							</a>
+						</li>
+						<li>
+							<a href="{{ route('admin.music-plugins.index') }}">
+								{{ trans('admin.navbar.music_plugins') }}
+							</a>
+						</li>
+						<li>
+							<a href="{{ route('admin.music-plugin-banks.index') }}">
+								{{ trans('admin.navbar.music_plugin_banks') }}
+							</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="{{ route('admin.music-genres.index') }}">
+						{{ trans('admin.navbar.music_genres') }}
+					</a>
+				</li>
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="">
+						{{ trans('admin.navbar.resources') }} <b class="caret"></b>
+					</a>
+					<ul class="dropdown-menu">
+						<li>
+							<a href="{{ route('admin.resource-files.index') }}">
+								{{ trans('admin.navbar.resource_files') }}
+							</a>
+						</li>
+						<li>
+							<a href="{{ route('admin.resource-locations.index') }}">
+								{{ trans('admin.navbar.resource_locations') }}
+							</a>
+						</li>
+					</ul>
+				</li>
 			</ul>
 
 			<ul class="nav navbar-nav navbar-right">
@@ -26,11 +80,14 @@
 					<a href="/">{{ trans('admin.navbar.frontend') }}</a>
 				</li>
 				<li class="dropdown">
-					<a class="dropdown-toggle" href="" data-toggle="dropdown">{{ $user->username }} <b
-							class="caret"></b></a>
+					<a class="dropdown-toggle" href="" data-toggle="dropdown">
+						{{ $user->username }} <b class="caret"></b>
+					</a>
 					<ul class="dropdown-menu">
 						<li>
-							<a href="{{ route('user.dashboard', ['username' => $user->username]) }}">{{ trans('navbar.dashboard') }}</a>
+							<a href="{{ route('user.dashboard', ['username' => $user->username]) }}">
+								{{ trans('navbar.dashboard') }}
+							</a>
 						</li>
 						<li class="divider"></li>
 						<li>
