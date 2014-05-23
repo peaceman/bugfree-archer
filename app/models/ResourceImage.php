94339c1e05016902f3fa7c814d9132e61a3b314b
peaceman <nixalio@gmail.com>
2014-05-23 10:15:10 +0200

    resource image and resource image file model classes

---
+++ app/models/ResourceImage.php
@@ -0,0 +1,38 @@
+<?php
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class ResourceImage
+ *
+ * @property int $id
+ * @property int $origin_resource_file_id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ *
+ * @property ResourceFile $originResourceFile
+ * @property ResourceImageFile[] $resourceImageFiles
+ * @property ResourceFile[] $resourceFiles
+ */
+class ResourceImage extends Eloquent
+{
+	public static $validationRules = [
+		'origin_resource_file_id' => ['required', 'exists:resource_files,id'],
+	];
+	protected $table = 'resource_images';
+
+	public function originResourceFile()
+	{
+		return $this->belongsTo('ResourceFile', 'origin_resource_file_id');
+	}
+
+	public function resourceImageFiles()
+	{
+		return $this->hasMany('ResourceImageFile');
+	}
+
+	public function resourceFiles()
+	{
+		return $this->hasManyThrough('ResourceFile', 'ResourceImageFile');
+	}
+}
---
+++ app/models/ResourceImageFile.php
@@ -0,0 +1,36 @@
+<?php
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class ResourceImageFile
+ *
+ * @property int $id
+ * @property int $resource_image_id
+ * @property int $resource_file_id
+ * @property string $image_format_identifier
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ *
+ * @property ResourceImage $resourceImage
+ * @property ResourceFile $resourceFile
+ */
+class ResourceImageFile extends Eloquent
+{
+	public static $validationRules = [
+		'resource_image_id' => ['required', 'exists:resource_images,id'],
+		'resource_file_id' => ['required', 'exists:resource_files,id'],
+		'image_format_identifier' => ['required'],
+	];
+	protected $table = 'resource_image_files';
+
+	public function resourceImage()
+	{
+		return $this->belongsTo('ResourceImage');
+	}
+
+	public function resourceFile()
+	{
+		return $this->belongsTo('ResourceFile');
+	}
+}
